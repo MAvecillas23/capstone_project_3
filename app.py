6c8d71122ce5cf0f376c6f191958c4f19bc016bf
Solis13 <156252616+Solis13@users.noreply.github.com>
2024-10-29 11:45:55 -0500

    Changed name Flood_api to api_flood

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template
 import geocoding as gc
-import Flood_API 
+import api_flood 
 # import apis and db module here
 
 # initialize Flask app object
@@ -17,7 +17,7 @@ def get_all_data():
     location = request.args.get('location')
     lat, long = gc.getCoordinates(location)
 
-    flood_info = Flood_API.get_flood_risk ( lat, long )
+    flood_info = api_flood.get_flood_risk ( lat, long )
     
 
 
