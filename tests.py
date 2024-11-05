928cbdf04a30b54198e14770c9f52bd2ecc0153b
Elijah Kamali <94736197+littlestripes@users.noreply.github.com>
2024-11-05 11:55:04 -0600

    Add unit tests for other APIs

--- tests.py
+++ tests.py
@@ -5,6 +5,8 @@ from unittest.mock import patch, Mock
 
 import geocoding as gc
 import air_pollution_api as ap
+import earthquake_api as eq
+import api_flood as af
 
 
 class TestGeocoding(TestCase):
@@ -49,10 +51,12 @@ class TestAirQualityAPI(TestCase):
 
     @patch("air_pollution_api.requests.get")
     def test_invalid_request(self, mock_get):
-        """ Test if an invalid request raises AirPollutionAPIError. """
+        """Test if an invalid request raises AirPollutionAPIError."""
         mock_response = Mock()
         mock_response.json.return_value = self.mock_response_err
-        mock_response.raise_for_status.side_effect = requests.HTTPError("404 Client Error")
+        mock_response.raise_for_status.side_effect = requests.HTTPError(
+            "404 Client Error"
+        )
         mock_get.return_value = mock_response
 
         coordinates = [0, 0]
@@ -71,6 +75,66 @@ class TestAirQualityAPI(TestCase):
             ap.get_air_pollution(coordinates)
 
         self.assertIn("Did you set OWM_API_KEY?", str(context.exception))
+
+
+class TestEarthquakeAPI(TestCase):
+    # NOTE: set environment variables before running any API test cases!
+
+    @classmethod
+    def setUpClass(cls):
+        cls.coordinates = [45, -93]
+        cls.mock_response_ok = {
+            "metadata": {"count": 2},
+            "features": [
+                {
+                    "properties": {
+                        "place": "128km SSW of Poland",
+                        "mag": 2.4,
+                        "time": 1388620296020,
+                    }
+                }
+            ],
+        }
+        cls.mock_good_data = [
+            "Location: 128km SSW of Poland Date: 2014-01-01Magnitude: 2.4"
+        ]
+
+    @patch("earthquake_api.requests.get")
+    def test_valid_request(self, mock_get):
+        """Test if a valid request to the earthquake API receives
+        a valid response.
+        """
+        mock_response = Mock()
+        mock_response.json.return_value = self.mock_response_ok
+        mock_response.raise_for_status = Mock()
+        mock_get.return_value = mock_response
+
+        response = eq.earthquake_main(*self.coordinates)
+
+        self.assertEqual(response, self.mock_good_data)
+
+
+class TestFloodAPI(TestCase):
+    # NOTE: set environment variables before running any API test cases!
+
+    @classmethod
+    def setUpClass(cls):
+        cls.coordinates = [45, -93]
+        cls.mock_response_ok = {"result": {"flood.s_fld_haz_ar": [{"fld_zone": "A"}]}}
+
+    @patch("api_flood.requests.get")
+    def test_valid_request(self, mock_get):
+        """Test if a valid request to the earthquake API receives
+        a valid response.
+        """
+        mock_response = Mock()
+        mock_response.json.return_value = self.mock_response_ok
+        mock_response.raise_for_status = Mock()
+        mock_get.return_value = mock_response
+
+        response = af.get_flood_risk(*self.coordinates)
+
+        self.assertEqual(response, "not safe")
 
 
 if __name__ == "__main__":
